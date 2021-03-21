03f5d8d151a5ba99d300b28a8ebeb187da549017
samson kimani <samsonkimani43@gmail.com>
2021-03-21 10:16:15 +0000

    Added styles to the flash messages and added a custom url

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,8 @@ class ContactsController < ApplicationController
             flash[:success] = "Message sent"
             redirect_to new_contact_path
         else
-            flash[:error]=@contact.errors.full_messages.join(", ")
-            redirect_to new_contact_path, notice: 'Error occured'
+            flash[:danger]=@contact.errors.full_messages.join(", ")
+            redirect_to new_contact_path
         end
     end
     private
